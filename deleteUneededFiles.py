b88f1048affd448358ad0fa2cee2ca78c4a2f2c1
Stephen Lang <stephenlang91@gmail.com>
2020-05-01 00:21:20 -0400

    adjusted file size

--- deleteUneededFiles.py
+++ deleteUneededFiles.py
@@ -2,16 +2,15 @@
 #deleteUneededFiles.py - Walks through a folder tree and prints the names of files that
 #are over 100MB
 
-#Calling os.path.getsize(path) will return the size in bytes of the file in the
-#  path argument.
 import os
 
-path = os.path.abspath('../../../..')
+path = os.path.abspath('../../../../..')
+print(path)
 for folderName, subfolders, filenames in os.walk(path):
     for filename in filenames:
-        if os.path.getsize(os.path.join(folderName, filename)) > 100:
+        if os.path.getsize(os.path.join(folderName, filename)) > (100*1000000):
             sizeMB = os.path.getsize(os.path.join(folderName, filename)) / 1000000
-            print(filename + ' : ' + str(sizeMB))
+            print(folderName + filename + ' : ' + str(sizeMB))
         else:
             continue
       
